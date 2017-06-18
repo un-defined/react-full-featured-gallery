d9ec7d3dafc49124ce314f16fe29e1a9dfd2d17e
un-defined <659168590@qq.com>
2017-06-18 23:00:53 +0800

    reconstruct  directory structure

---
+++ src/Gallery.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { css, StyleSheet } from 'aphrodite/no-important';
+import ScrollLock from 'react-scrolllock';
+
+class Gallery extends Component {
+
+}
+
+export default Gallery;
---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+import deepMerge from './deepMerge';
+
+module.exports = {
+    deepMerge
+}
